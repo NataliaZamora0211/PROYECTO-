187ac1503cd553ca09c0ca77d74998dd72d2120e
NataliaZamora0211 <148570852+NataliaZamora0211@users.noreply.github.com>
2023-12-01 12:24:26 -0500

    Add files via upload

---
+++ sensorligometro.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int lectura = analogRead(A0);
+
+Serial.print("La lectura es:");
+Serial.println(lectura);
+
+//lecturas entre 1000-1023
+if (lectura >= 1000) {
+  Serial .println(">> EL SENSOR ESTA DESCONECTADO O FUERA DEL SUELO <<") ;
+}
+else if (lectura<1000 && lectura >=600) {
+  Serial.println(">> EL SUELO ESTA SECO <<");
+  
+}
+else if (lectura<600 && lectura >=370) { 
+  Serial.println(">> EL SUELO ESTA HUMEDO <<");
+}
+else if (lectura<370) { 
+  Serial.println(">> EL SUELO ESTA PRACTICAMENTE EN AGUA <<");
+}
+delay(1000);
+}
